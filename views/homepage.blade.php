a8535f460aa08b7da3a2188fa07d8be601199a7e
James Wilson <james.wilson@cap2.co.uk>
2019-06-17 11:15:06 +0100

    Update homepage.blade.php

--- views/homepage.blade.php
+++ views/homepage.blade.php
@@ -7,8 +7,8 @@
 <center>
   <h1>Colour:  {{ $data['hex'] }} </h1>
   <h1>Name: {{ $data['colour'] }}</h1>
-    <a href="{{ $data['links']['self'] }}"><img src="{{ $data['images']['regular'] }}" width="500" height="auto"></a>
-    <p> Image by : {{ $data['user']['name'] }} </a>
+    <a href="{{ $data['links']['html'] }}"><img src="{{ $data['images']['regular'] }}" width="500" height="auto"></a> </a>
+    <p> Image by : <a href="{{ $data['user']['links']['html'] }}">{{ $data['user']['name'] }}</a> on <a href="https://unsplash.com/?utm_source=Colour_Picker&utm_medium=referral">Unsplash</a></p>
 </center>
 
 
