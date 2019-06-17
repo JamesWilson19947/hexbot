d307db23869fe4e99dc0e1490a462819bd949ce9
James Wilson <james.wilson@cap2.co.uk>
2019-06-17 08:50:17 +0100

    Update

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ use Jenssegers\Blade\Blade;
 
 # Load Composer
 require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
 $blade = new Blade('views', 'cache');
 
 $json = file_get_contents("http://api.noopschallenge.com/hexbot");
@@ -11,12 +14,14 @@ $data = array();
 $data = json_decode($json, true);
 $hex = $data['colors'][0]['value'];
 
+
 $colour = new Colour\ColourFind;
 
 $data = [];
 $data['colour'] = $colour->returnName($hex);
 $data['hex'] = $hex;
 
+echo $_ENV['TEST'];
 echo $blade->make('homepage')->with('data', $data);
 
 
